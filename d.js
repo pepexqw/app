709c83a35b284abd40db9d3935b4cd2d0f7e3d67
pepexqw <superampleev@gmail.com>
2025-12-31 20:12:24 +0300

    Обновление форума

--- d.js
+++ d.js
@@ -7,7 +7,15 @@
       "content": "TEST",
       "timestamp": 1767201130,
       "date": "2025-12-31 17:12:10",
-      "comments": []
+      "comments": [
+        {
+          "id": 1767201144389,
+          "author": "TEST",
+          "content": "TEST",
+          "timestamp": 1767201144,
+          "date": "2025-12-31 17:12:24"
+        }
+      ]
     }
   ]
 }
