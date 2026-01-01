177e0649999541c3a0595bc2d264de1eb36d3460
pepexqw <superampleev@gmail.com>
2026-01-01 11:16:54 +0300

    Update from forum 08:16 01.01.2026

--- d.js
+++ d.js
@@ -1,1 +1,19 @@
-
+var data = {
+  "topics": [
+    {
+      "id": 1,
+      "title": "test",
+      "author": "test",
+      "book": "test",
+      "message": "test",
+      "date": "01.01.2026 08:16",
+      "comments": []
+    }
+  ],
+  "books": [
+    {
+      "name": "test",
+      "count": 1
+    }
+  ]
+};
