e8d7bceaca7fa0cc329c4507608d76c84f1d2d07
pepexqw <superampleev@gmail.com>
2026-01-02 21:43:15 +0300

    Обновление данных

--- d.js
+++ d.js
@@ -3,5 +3,10 @@
     "id": 1,
     "username": "pepexqw",
     "password_hash": "2c622bdc9e7e3510158f36178ec7aea08558bec185d8dc2d11e15d463ddf96b0"
+  },
+  {
+    "id": 2,
+    "username": "pepexqw1",
+    "password_hash": "7ed7cdd96431457a8e038fe9457c96f604ceb87d0fbeeb389121ff297efea002"
   }
 ]
