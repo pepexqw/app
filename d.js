bb61f0eb5aafaa40be42681751453bd3fb6d93a3
pepexqw <superampleev@gmail.com>
2026-01-03 17:23:48 +0300

    Update d.js

--- d.js
+++ d.js
@@ -1,12 +1,1 @@
-[
-  {
-    "id": 1,
-    "username": "pepexqw",
-    "password_hash": "2c622bdc9e7e3510158f36178ec7aea08558bec185d8dc2d11e15d463ddf96b0"
-  },
-  {
-    "id": 2,
-    "username": "tewtewwet",
-    "password_hash": "275dc69e91a8f83db106f82177453c5fb41a3953b06654ba3de39548f52f15f4"
-  }
-]
+
