0250a3c4fe7eca46468ac8b269e2927d7f64a5a3
pepexqw <superampleev@gmail.com>
2026-01-03 18:29:31 +0300

    Обновление данных

--- d.js
+++ d.js
@@ -5,5 +5,12 @@
     "password_hash": "2c622bdc9e7e3510158f36178ec7aea08558bec185d8dc2d11e15d463ddf96b0",
     "ip": "80.251.225.243",
     "registration_date": "2026-01-03T14:26:35.781480"
+  },
+  {
+    "id": 2,
+    "username": "Мария",
+    "password_hash": "654e24366ecb1dd98a27f3af017766d2f19d0345036d763c9b2028d52f7fe232",
+    "ip": "92.36.96.149",
+    "registration_date": "2026-01-03T15:29:30.963045"
   }
 ]
